c19c9c982f209bb46473d0c1076a77d59e477cfc
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2024-03-27 14:49:29 -0400

    Add files via upload

--- inc/template-newly-added-books.php
+++ inc/template-newly-added-books.php
@@ -1,4 +1,4 @@
-<?php get_header(); 
+<?php get_header();
 global $wpdb;
 $books_table = $wpdb->prefix . "tomc_books";
 $book_genres_table = $wpdb->prefix .  "tomc_book_genres";
@@ -7,7 +7,7 @@ $posts_table = $wpdb->prefix . "posts";
 $product_types_table = $wpdb->prefix . "tomc_product_types";
 $pen_names_table = $wpdb->prefix . "tomc_pen_names_books";
 
-$query = 'select distinct b.id, b.title, b.product_image_id, f.post_title as pen_name, b.book_description, b.createdate, g.id as product_url
+$query = 'select distinct b.id, b.title,f.post_title as pen_name, b.book_description, b.createdate, g.id as product_url
         from %i b
         join %i c on b.id = c.bookid
         join %i d on c.typeid = d.id
@@ -16,7 +16,7 @@ $query = 'select distinct b.id, b.title, b.product_image_id, f.post_title as pen
         join %i g on c.productid = g.id
         where d.type_name = %s
         order by b.createdate desc
-        limit 300';
+        limit 100';
 $results = $wpdb->get_results($wpdb->prepare($query, $books_table, $book_products_table, $product_types_table, $pen_names_table, $posts_table, $posts_table, 'ebook'), ARRAY_A);
 
 ?><main class="half-screen">
@@ -29,9 +29,10 @@ $results = $wpdb->get_results($wpdb->prepare($query, $books_table, $book_product
             if ($index == 2){
                 ?><div class="tomc-book-organization--new-book-2">
                     <div class="tomc-browse--search-result-top-section">
-                        <h2><?php echo $results[$index]['title']; ?></h2>
-                        <h3><?php echo $results[$index]['pen_name'] ? 'by ' . $results[$index]['pen_name'] : 'by unknown or anonymous author'; ?></h3>
-                        <!-- <img class="book-cover--small" alt="<?php echo 'book cover for ' . $results[$index]['title']; ?>" src="<?php echo get_the_post_thumbnail_url($results[$index]['product_image_id']); ?>" /> -->
+                        <a href="<?php echo get_permalink($results[$index]['product_url']); ?>">
+                            <h3><?php echo $results[$index]['title']; ?></h2>
+                        </a>
+                        <h4><?php echo $results[$index]['pen_name'] ? 'by ' . $results[$index]['pen_name'] : 'by unknown or anonymous author'; ?></h3>
                     </div>
                     <div class="tomc-browse--new-book-bottom-section">
                         <p><?php echo $results[$index]['book_description'].substr(0, 500) . '...'; ?></p>
@@ -43,9 +44,10 @@ $results = $wpdb->get_results($wpdb->prepare($query, $books_table, $book_product
             else if ($index % 3 == 0){
                 ?><div class="tomc-book-organization--new-book-3">
                     <div class="tomc-browse--search-result-top-section">
-                        <h2><?php echo $results[$index]['title']; ?></h2>
-                        <h3><?php echo $results[$index]['pen_name'] ? 'by ' . $results[$index]['pen_name'] : 'by unknown or anonymous author'; ?></h3>
-                        <!-- <img class="book-cover--small" alt="<?php echo 'book cover for ' . $results[$index]['title']; ?>" src="<?php echo get_the_post_thumbnail_url($results[$index]['product_image_id']); ?>" /> -->
+                        <a href="<?php echo get_permalink($results[$index]['product_url']); ?>">
+                            <h3><?php echo $results[$index]['title']; ?></h2>
+                        </a>
+                        <h4><?php echo $results[$index]['pen_name'] ? 'by ' . $results[$index]['pen_name'] : 'by unknown or anonymous author'; ?></h3>
                     </div>
                     <div class="tomc-browse--new-book-bottom-section">
                         <p><?php echo $results[$index]['book_description'].substr(0, 500) . '...'; ?></p>
@@ -54,9 +56,10 @@ $results = $wpdb->get_results($wpdb->prepare($query, $books_table, $book_product
             <?php } else if (($index + 1) % 3 == 0){
                 ?><div class="tomc-book-organization--new-book-2">
                 <div class="tomc-browse--search-result-top-section">
-                    <h2><?php echo $results[$index]['title']; ?></h2>
-                    <h3><?php echo $results[$index]['pen_name'] ? 'by ' . $results[$index]['pen_name'] : 'by unknown or anonymous author'; ?></h3>
-                    <!-- <img class="book-cover--small" alt="<?php echo 'book cover for ' . $results[$index]['title']; ?>" src="<?php echo get_the_post_thumbnail_url($results[$index]['product_image_id']); ?>" /> -->
+                    <a href="<?php echo get_permalink($results[$index]['product_url']); ?>">
+                            <h3><?php echo $results[$index]['title']; ?></h2>
+                        </a>
+                    <h4><?php echo $results[$index]['pen_name'] ? 'by ' . $results[$index]['pen_name'] : 'by unknown or anonymous author'; ?></h3>
                 </div>
                 <div class="tomc-browse--new-book-bottom-section">
                     <p><?php echo $results[$index]['book_description'].substr(0, 500) . '...'; ?></p>
@@ -65,9 +68,10 @@ $results = $wpdb->get_results($wpdb->prepare($query, $books_table, $book_product
             <?php } else {
                 ?><div class="tomc-book-organization--new-book-1">
                     <div class="tomc-browse--search-result-top-section">
-                        <h2><?php echo $results[$index]['title']; ?></h2>
-                        <h3><?php echo $results[$index]['pen_name'] ? 'by ' . $results[$index]['pen_name'] : 'by unknown or anonymous author'; ?></h3>
-                        <!-- <img class="book-cover--small" alt="<?php echo 'book cover for ' . $results[$index]['title']; ?>" src="<?php echo get_the_post_thumbnail_url($results[$index]['product_image_id']); ?>" /> -->
+                        <a href="<?php echo get_permalink($results[$index]['product_url']); ?>">
+                            <h3><?php echo $results[$index]['title']; ?></h2>
+                        </a>
+                        <h4><?php echo $results[$index]['pen_name'] ? 'by ' . $results[$index]['pen_name'] : 'by unknown or anonymous author'; ?></h3>
                     </div>
                     <div class="tomc-browse--new-book-bottom-section">
                         <p><?php echo $results[$index]['book_description'].substr(0, 500) . '...'; ?></p>
@@ -78,7 +82,7 @@ $results = $wpdb->get_results($wpdb->prepare($query, $books_table, $book_product
         </div>  <!-- tomc-book-organization--newly-added-continued-ebooks -->
     </div>
 
-    <?php $query = 'select distinct b.id, b.title, b.product_image_id, f.post_title as pen_name, b.book_description, b.createdate, g.id as product_url
+    <?php $query = 'select distinct b.id, b.title, f.post_title as pen_name, b.book_description, b.createdate, g.id as product_url
         from %i b
         join %i c on b.id = c.bookid
         join %i d on c.typeid = d.id
@@ -87,7 +91,7 @@ $results = $wpdb->get_results($wpdb->prepare($query, $books_table, $book_product
         join %i g on c.productid = g.id
         where d.type_name = %s
         order by b.createdate desc
-        limit 300';
+        limit 100';
     $results = $wpdb->get_results($wpdb->prepare($query, $books_table, $book_products_table, $product_types_table, $pen_names_table, $posts_table, $posts_table, 'audiobook'), ARRAY_A);
 
     ?><div class="sub-banner--slim">
@@ -98,9 +102,10 @@ $results = $wpdb->get_results($wpdb->prepare($query, $books_table, $book_product
             if ($index == 2){
                 ?><div class="tomc-book-organization--new-book-2">
                     <div class="tomc-browse--search-result-top-section">
-                        <h2><?php echo $results[$index]['title']; ?></h2>
-                        <h3><?php echo $results[$index]['pen_name'] ? 'by ' . $results[$index]['pen_name'] : 'by unknown or anonymous author'; ?></h3>
-                        <!-- <img class="book-cover--small" alt="<?php echo 'book cover for ' . $results[$index]['title']; ?>" src="<?php echo get_the_post_thumbnail_url($results[$index]['product_image_id']); ?>" /> -->
+                        <a href="<?php echo get_permalink($results[$index]['product_url']); ?>">
+                            <h3><?php echo $results[$index]['title']; ?></h2>
+                        </a>
+                        <h4><?php echo $results[$index]['pen_name'] ? 'by ' . $results[$index]['pen_name'] : 'by unknown or anonymous author'; ?></h3>
                     </div>
                     <div class="tomc-browse--new-book-bottom-section">
                         <p><?php echo $results[$index]['book_description'].substr(0, 500) . '...'; ?></p>
@@ -112,9 +117,10 @@ $results = $wpdb->get_results($wpdb->prepare($query, $books_table, $book_product
             else if ($index % 3 == 0){
                 ?><div class="tomc-book-organization--new-book-3">
                     <div class="tomc-browse--search-result-top-section">
-                        <h2><?php echo $results[$index]['title']; ?></h2>
-                        <h3><?php echo $results[$index]['pen_name'] ? 'by ' . $results[$index]['pen_name'] : 'by unknown or anonymous author'; ?></h3>
-                        <!-- <img class="book-cover--small" alt="<?php echo 'book cover for ' . $results[$index]['title']; ?>" src="<?php echo get_the_post_thumbnail_url($results[$index]['product_image_id']); ?>" /> -->
+                        <a href="<?php echo get_permalink($results[$index]['product_url']); ?>">
+                            <h3><?php echo $results[$index]['title']; ?></h2>
+                        </a>
+                        <h4><?php echo $results[$index]['pen_name'] ? 'by ' . $results[$index]['pen_name'] : 'by unknown or anonymous author'; ?></h3>
                     </div>
                     <div class="tomc-browse--new-book-bottom-section">
                         <p><?php echo $results[$index]['book_description'].substr(0, 500) . '...'; ?></p>
@@ -123,9 +129,10 @@ $results = $wpdb->get_results($wpdb->prepare($query, $books_table, $book_product
             <?php } else if (($index + 1) % 3 == 0){
                 ?><div class="tomc-book-organization--new-book-2">
                 <div class="tomc-browse--search-result-top-section">
-                    <h2><?php echo $results[$index]['title']; ?></h2>
-                    <h3><?php echo $results[$index]['pen_name'] ? 'by ' . $results[$index]['pen_name'] : 'by unknown or anonymous author'; ?></h3>
-                    <!-- <img class="book-cover--small" alt="<?php echo 'book cover for ' . $results[$index]['title']; ?>" src="<?php echo get_the_post_thumbnail_url($results[$index]['product_image_id']); ?>" /> -->
+                    <a href="<?php echo get_permalink($results[$index]['product_url']); ?>">
+                        <h3><?php echo $results[$index]['title']; ?></h2>
+                    </a>
+                    <h4><?php echo $results[$index]['pen_name'] ? 'by ' . $results[$index]['pen_name'] : 'by unknown or anonymous author'; ?></h3>
                 </div>
                 <div class="tomc-browse--new-book-bottom-section">
                     <p><?php echo $results[$index]['book_description'].substr(0, 500) . '...'; ?></p>
@@ -134,9 +141,10 @@ $results = $wpdb->get_results($wpdb->prepare($query, $books_table, $book_product
             <?php } else {
                 ?><div class="tomc-book-organization--new-book-1">
                     <div class="tomc-browse--search-result-top-section">
-                        <h2><?php echo $results[$index]['title']; ?></h2>
-                        <h3><?php echo $results[$index]['pen_name'] ? 'by ' . $results[$index]['pen_name'] : 'by unknown or anonymous author'; ?></h3>
-                        <!-- <img class="book-cover--small" alt="<?php echo 'book cover for ' . $results[$index]['title']; ?>" src="<?php echo get_the_post_thumbnail_url($results[$index]['product_image_id']); ?>" /> -->
+                        <a href="<?php echo get_permalink($results[$index]['product_url']); ?>">
+                            <h3><?php echo $results[$index]['title']; ?></h2>
+                        </a>
+                        <h4><?php echo $results[$index]['pen_name'] ? 'by ' . $results[$index]['pen_name'] : 'by unknown or anonymous author'; ?></h3>
                     </div>
                     <div class="tomc-browse--new-book-bottom-section">
                         <p><?php echo $results[$index]['book_description'].substr(0, 500) . '...'; ?></p>
