223cd5c66600dbb7d4fc907e38ded323c32fca47
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2024-04-21 16:46:38 -0400

    Add files via upload

--- inc/template-pen-names.php
+++ inc/template-pen-names.php
@@ -1,5 +1,4 @@
 <?php global $wpdb;
-$user_pennames_table = $wpdb->prefix . "tomc_user_pennames";
 $posts_table = $wpdb->prefix . "posts";
 $userid = get_current_user_id();
 $user = wp_get_current_user();
@@ -10,17 +9,20 @@ get_header();
     <?php if (is_user_logged_in()){
         if (in_array( 'dc_vendor', (array) $user->roles ) ){
             ?><div class="banner"><h1 class="centered-text">Manage Your Author Names</h1></div>
-            <?php $query = 'select p.id, p.post_title, p.post_content from %i upn join %i p on upn.pennameid = p.id where upn.userid = %d';
-            $names = $wpdb->get_results($wpdb->prepare($query, $user_pennames_table, $posts_table, $userid));
+            <p class="centered-text"><strong>Please add any name you publish books under, whether it's your legal name or a pen name.</strong></p>
+            <?php $query = 'select p.id, p.post_title, p.post_content from %i p where p.post_type = "author-profile" and p.post_author = %d';
+            $names = $wpdb->get_results($wpdb->prepare($query, $posts_table, $userid));
             if ($names){
                 ?><div class="third-screen">
                     <?php foreach($names as $name){
-                        ?><div class="tomc-book-organization--book-to-edit page-accent-alt-thin">
-                            <h2 class="centered-text"><?php echo $name->id; ?></h2>
+                        ?><div class="tomc-book-organization--book-to-edit page-accent-alt-thin" data="<?php echo 'pen-name-' . $name->id; ?>">
+                            <h2 class="centered-text"><?php echo $name->post_title; ?></h2>
+                            <p class="generic-content"><?php echo $name->post_content; ?></p>
+                            <button class="tomc-book-organization--save-button tomc-book-organization--edit-bio">edit bio</button>
                         </div>
                     <?php }
                 ?></div>                    
-            <?php }
+            <?php } 
         } else {
             ?><div>
                 <p class="centered-text">Only authors can add and edit author names.</p>
