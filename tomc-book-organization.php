45f7b949c8eb77b7976264c789fbd13cd2b6099b
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2024-06-11 18:56:24 -0400

    Add files via upload

--- tomc-book-organization.php
+++ tomc-book-organization.php
@@ -41,9 +41,9 @@ class TOMCBookOrganizationPlugin {
             'root_url' => get_site_url()
         ));
 
-        // add_action('activate_tomc-book-organization/tomc-book-organization.php', array($this, 'onActivate'));
+        add_action('activate_tomc-book-organization/tomc-book-organization.php', array($this, 'onActivate'));
         // register_activation_hook( __FILE__, array($this, 'onActivate'));
-        add_action('init', array($this, 'onActivate'));
+        // add_action('init', array($this, 'onActivate'));
         add_action('init', array($this, 'registerScripts'));
         add_action('wp_enqueue_scripts', array($this, 'pluginFiles'));
         add_filter('template_include', array($this, 'loadTemplate'), 99);
