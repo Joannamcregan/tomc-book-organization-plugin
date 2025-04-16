c9da0ea3b8932ba3e0f400a5facfe30966df263d
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2025-04-16 17:48:03 -0400

    Add files via upload

--- inc/template-add-book.php
+++ inc/template-add-book.php
@@ -305,9 +305,9 @@ get_header();
             </div>
         <?php } else {
             ?><div class="half-screen">
-                <p class="centered-text">Only authors can create books.</p>
-                <p class="centered-text">Looking for books you've purchased? You can find your ebook and audiobook downloads <a href="<?php echo esc_url(site_url('/my-account/downloads'));?>">here</a>.</p>
-                <p class="centered-text">As a reader, you can also create and manage <a href="<?php echo esc_url(site_url('/my-bookshelves')); ?>">bookshelves</a>.</p>
+                <p class="centered-text padding-x-20">Only authors can create books.</p>
+                <p class="centered-text padding-x-20">Looking for books you've purchased? You can find your ebook and audiobook downloads <a href="<?php echo esc_url(site_url('/my-account/downloads'));?>">here</a>.</p>
+                <p class="centered-text padding-x-20">As a reader, you can also create and manage <a href="<?php echo esc_url(site_url('/my-bookshelves')); ?>">bookshelves</a>.</p>
             </div>
         <?php }
     } else {
